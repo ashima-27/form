5b2bc15215f1ce463d7d8d62903970be2bc8181a
duggalashima905@gmail.com <duggalashima905@gmail.com>
2025-01-23 18:49:39 +0530

    ew

--- src/app/components/molecules/ImageHeadingSpanCard.js
+++ src/app/components/molecules/ImageHeadingSpanCard.js
@@ -5,15 +5,14 @@ import { Heading, Span } from "../atoms/index";
 const ImageHeadingSpanCard = ({ imageSrc, headingText, spanText, altText }) => {
   return (
     <div className="max-w-xs rounded-lg p-4">
-      <div className="relative aspect-video w-full sm:aspect-square md:aspect-[4/3] lg:aspect-[3/2] xl:aspect-[16/9]">
+      <div className="relative aspect-video w-full my-4 pt-6 md:pt-0 md:my-0 sm:aspect-square md:aspect-[4/3] lg:aspect-[2/2] xl:aspect-[12/9]">
         {" "}
         <Image
           src={imageSrc}
           alt={altText}
           layout="fill"
           objectFit="contain"
-          className="object-contain transition-all hover:scale-105"
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          className="object-contain w-20 h-20 md:w-60 md:h-60"
         />
       </div>
 
